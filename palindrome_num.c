0a3670d01bbe57a89cd5c8e0cc4d9c97e5e58075
RubabaBinteMahbub <rubababintemahbub2007@gmail.com>
2025-10-09 19:37:21 +0600

    palindrome_num.c

---
+++ palindrome_num.c
@@ -0,0 +1,25 @@
+// Palindrome numbers are numbers with
+mirror image such as 121, 474, 323 etc.
+  
+#include<stdio.h>
+int main(){
+    int num=1234,rev=0,rem;
+    printf("Number = %d\n",num);
+    int original_num=num;
+
+    while(num!=0){
+       rem=num%10;
+       rev=rev*10+rem;
+       num/=10;
+    }
+   printf("Reversed number = %d\n",rev);
+
+    if(original_num==rev){
+        printf("%d is a palindrome number.\n",original_num);
+    }
+    else{
+        printf("%d is not a palindrom number.\n",original_num);
+    }
+    
+    return 0;
+}
